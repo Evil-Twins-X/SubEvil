4d2ebda5190b5c6949dff71fde682762a7e707d1
Evil_Twins <100705835+Evil-Twins-X@users.noreply.github.com>
2022-06-18 16:29:37 -0400

    Update Brand.py

--- Lib/Brand.py
+++ Lib/Brand.py
@@ -1,5 +1,5 @@
 from colored import fg,attr
-def Brand():
+def Brand(v):
     listBrand = [f"""
     
                     ███████{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}   ██{fg(40)}╗{attr(0)}██████{fg(40)}╗ {attr(0)}███████{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}   ██{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}     
@@ -9,7 +9,7 @@ def Brand():
                     ███████{fg(40)}║{attr(0)}{fg(40)}╚{attr(0)}██████{fg(40)}╔╝{attr(0)}██████{fg(40)}╔╝{attr(0)}███████{fg(40)}╗{attr(0)} {fg(40)}╚{attr(0)}████{fg(40)}╔╝{attr(0)} ██{fg(40)}║{attr(0)}███████{fg(40)}╗{attr(0)}
                     {fg(40)}╚══════╝{attr(0)} {fg(40)}╚═════╝ ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝╚══════╝{attr(0)}
                  {fg(1)}#{attr(0)}{fg(40)}====================================================={attr(0)}{fg(1)}#{attr(0)}
-                 {fg(1)}#{attr(0)}           Tool  Version : V 1.0                     {fg(1)}#{attr(0)}
+                 {fg(1)}#{attr(0)}           Tool  Version : {v}                     {fg(1)}#{attr(0)}
                  {fg(1)}#{attr(0)}           Programmer    : EvilTwins                 {fg(1)}#{attr(0)}
                  {fg(1)}#{attr(0)}           Facebook      : @evi1.twins               {fg(1)}#{attr(0)}
                  {fg(1)}#{attr(0)}           Github        : @Evi1-Back                {fg(1)}#{attr(0)}
