5c0e40cecfb7beaf56ec875ed45221845e40e08d
Evil_Twins <100705835+Evil-Twins-X@users.noreply.github.com>
2023-07-31 02:14:59 -0700

    Update HttpORHttps.py

--- Lib/HttpORHttps.py
+++ Lib/HttpORHttps.py
@@ -2,7 +2,7 @@ from ast import Pass
 from posixpath import split
 import requests
 from lxml.html import fromstring
-from colored import fg,attr
+from colored import Fore,Style
 def Http(Target,HTTP,Title,StatusCode):
     try:
         if HTTP   == True:
@@ -16,75 +16,75 @@ def Http(Target,HTTP,Title,StatusCode):
                 try:
                     tree = fromstring(req.content)
                     tree.findtext('.//title')
-                    Pro.append(f"{fg(40)}[{attr(0)}{tree.findtext('.//title')}{fg(40)}]{attr(0)}")
+                    Pro.append(f"{Fore.green}[{Style.reset}{tree.findtext('.//title')}{Fore.green}]{Style.reset}")
                 except:
-                    Pro.append(f"{fg(2)}[{attr(0)} Not Title{fg(2)}]{attr(0)}")
+                    Pro.append(f"{Fore.Red}[{Style.reset} Not Title{Fore.Red}]{Style.reset}")
             if StatusCode == False :
                 pass 
             else:
                 status = req.status_code
                 if StatusCode == "All" or  StatusCode == "all":
                     if status  == 200:
-                        Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                     else:
-                        Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                 elif ',' in str(StatusCode):
                     s = StatusCode.split(',')
                     if len(s) == 2 :
                         if s[0] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[0] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[1] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[1] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                     if len(s) == 3 :
                         if s[0] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[0] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[1] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[1] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]") 
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]") 
                         if s[2] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[2] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                     if len(s) == 4 :
                         if s[0] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[0] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[1] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[1] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]") 
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]") 
                         if s[2] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[2] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[3] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[3] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                     if len(s) > 4 :
                         print("Can Use --status-code All")    
                 elif int(status) == int(StatusCode):
                     if status  == 200:
-                        Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                     else:
-                        Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                 else:
                     pass
             if len(Pro) == 3:
@@ -117,75 +117,75 @@ def Https(Target,HTTPs,Title,StatusCode):
                 try:
                     tree = fromstring(req.content)
                     tree.findtext('.//title')
-                    Pro.append(f"{fg(40)}[{attr(0)}{tree.findtext('.//title')}{fg(40)}]{attr(0)}")
+                    Pro.append(f"{Fore.green}[{Style.reset}{tree.findtext('.//title')}{Fore.green}]{Style.reset}")
                 except:
-                    Pro.append(f"{fg(2)}[{attr(0)} Not Title{fg(2)}]{attr(0)}")
+                    Pro.append(f"{Fore.Red}[{Style.reset} Not Title{Fore.Red}]{Style.reset}")
             if StatusCode == False :
                 pass 
             else:
                 status = req.status_code
                 if StatusCode == "All" or  StatusCode == "all":
                     if status  == 200:
-                        Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                     else:
-                        Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                 elif ',' in str(StatusCode):
                     s = StatusCode.split(',')
                     if len(s) == 2 :
                         if s[0] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[0] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[1] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[1] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                     if len(s) == 3 :
                         if s[0] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[0] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[1] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[1] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]") 
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]") 
                         if s[2] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[2] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                     if len(s) == 4 :
                         if s[0] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[0] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[1] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[1] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]") 
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]") 
                         if s[2] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[2] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                         if s[3] == str(status)  == "200":
-                            Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                            Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                         else:
                             if s[3] == str(status):
-                                Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                                Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                     if len(s) > 4 :
                         print("Can Use --status-code All")    
                 elif int(status) == int(StatusCode):
                     if status  == 200:
-                        Pro.append(f"[ {fg(40)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.green}{status}{Style.reset} ]")
                     else:
-                        Pro.append(f"[ {fg(1)}{status}{attr(0)} ]")
+                        Pro.append(f"[ {Fore.Red}{status}{Style.reset} ]")
                 else:
                     pass
             if len(Pro) == 3:
