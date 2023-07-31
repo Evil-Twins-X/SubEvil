f9622ae86d9868aaf13263cc0beba8d9eb156d15
Evil_Twins <100705835+Evil-Twins-X@users.noreply.github.com>
2023-07-31 02:14:43 -0700

    Update Brand.py

--- Lib/Brand.py
+++ Lib/Brand.py
@@ -1,19 +1,19 @@
-from colored import fg,attr
+from colored import Fore,Style
 def Brand(v):
     listBrand = [f"""
     
-                    ███████{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}   ██{fg(40)}╗{attr(0)}██████{fg(40)}╗ {attr(0)}███████{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}   ██{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}██{fg(40)}╗{attr(0)}     
-                    ██{fg(40)}╔════╝{attr(0)}██{fg(40)}║{attr(0)}   ██{fg(40)}║{attr(0)}██{fg(40)}╔══{attr(0)}██{fg(40)}╗{attr(0)}██{fg(40)}╔════╝{attr(0)}██{fg(40)}║ {attr(0)}  ██{fg(40)}║{attr(0)}██{fg(40)}║{attr(0)}██{fg(40)}║{attr(0)}     
-                    ███████{fg(40)}╗{attr(0)}██{fg(40)}║{attr(0)}   ██{fg(40)}║{attr(0)}██████{fg(40)}╔╝{attr(0)}█████{fg(40)}╗{attr(0)}  ██{fg(40)}║{attr(0)}   ██{fg(40)}║{attr(0)}██{fg(40)}║{attr(0)}██{fg(40)}║  {attr(0)}   
-                    {fg(40)}╚════{attr(0)}██{fg(40)}║{attr(0)}██{fg(40)}║{attr(0)}   ██{fg(40)}║{attr(0)}██{fg(40)}╔══{attr(0)}██{fg(40)}╗{attr(0)}██{fg(40)}╔══╝{attr(0)}  {fg(40)}╚{attr(0)}██{fg(40)}╗{attr(0)} ██{fg(40)}╔╝{attr(0)}██{fg(40)}║{attr(0)}██{fg(40)}║{attr(0)}     
-                    ███████{fg(40)}║{attr(0)}{fg(40)}╚{attr(0)}██████{fg(40)}╔╝{attr(0)}██████{fg(40)}╔╝{attr(0)}███████{fg(40)}╗{attr(0)} {fg(40)}╚{attr(0)}████{fg(40)}╔╝{attr(0)} ██{fg(40)}║{attr(0)}███████{fg(40)}╗{attr(0)}
-                    {fg(40)}╚══════╝{attr(0)} {fg(40)}╚═════╝ ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝╚══════╝{attr(0)}
-                 {fg(1)}#{attr(0)}{fg(40)}====================================================={attr(0)}{fg(1)}#{attr(0)}
-                 {fg(1)}#{attr(0)}           Tool  Version : {v}                    {fg(1)}#{attr(0)}
-                 {fg(1)}#{attr(0)}           Programmer    : EvilTwins                 {fg(1)}#{attr(0)}
-                 {fg(1)}#{attr(0)}           Facebook      : @evi1.twins               {fg(1)}#{attr(0)}
-                 {fg(1)}#{attr(0)}           Github        : @Evi1-Back                {fg(1)}#{attr(0)}
-                 {fg(1)}#{attr(0)}{fg(40)}====================================================={attr(0)}{fg(1)}#{attr(0)}
+                    ███████{Fore.green}╗{Style.reset}██{Fore.green}╗{Style.reset}   ██{Fore.green}╗{Style.reset}██████{Fore.green}╗ {Style.reset}███████{Fore.green}╗{Style.reset}██{Fore.green}╗{Style.reset}   ██{Fore.green}╗{Style.reset}██{Fore.green}╗{Style.reset}██{Fore.green}╗{Style.reset}     
+                    ██{Fore.green}╔════╝{Style.reset}██{Fore.green}║{Style.reset}   ██{Fore.green}║{Style.reset}██{Fore.green}╔══{Style.reset}██{Fore.green}╗{Style.reset}██{Fore.green}╔════╝{Style.reset}██{Fore.green}║ {Style.reset}  ██{Fore.green}║{Style.reset}██{Fore.green}║{Style.reset}██{Fore.green}║{Style.reset}     
+                    ███████{Fore.green}╗{Style.reset}██{Fore.green}║{Style.reset}   ██{Fore.green}║{Style.reset}██████{Fore.green}╔╝{Style.reset}█████{Fore.green}╗{Style.reset}  ██{Fore.green}║{Style.reset}   ██{Fore.green}║{Style.reset}██{Fore.green}║{Style.reset}██{Fore.green}║  {Style.reset}   
+                    {Fore.green}╚════{Style.reset}██{Fore.green}║{Style.reset}██{Fore.green}║{Style.reset}   ██{Fore.green}║{Style.reset}██{Fore.green}╔══{Style.reset}██{Fore.green}╗{Style.reset}██{Fore.green}╔══╝{Style.reset}  {Fore.green}╚{Style.reset}██{Fore.green}╗{Style.reset} ██{Fore.green}╔╝{Style.reset}██{Fore.green}║{Style.reset}██{Fore.green}║{Style.reset}     
+                    ███████{Fore.green}║{Style.reset}{Fore.green}╚{Style.reset}██████{Fore.green}╔╝{Style.reset}██████{Fore.green}╔╝{Style.reset}███████{Fore.green}╗{Style.reset} {Fore.green}╚{Style.reset}████{Fore.green}╔╝{Style.reset} ██{Fore.green}║{Style.reset}███████{Fore.green}╗{Style.reset}
+                    {Fore.green}╚══════╝{Style.reset} {Fore.green}╚═════╝ ╚═════╝ ╚══════╝  ╚═══╝  ╚═╝╚══════╝{Style.reset}
+                 {Fore.red}#{Style.reset}{Fore.green}====================================================={Style.reset}{Fore.red}#{Style.reset}
+                 {Fore.red}#{Style.reset}           Tool  Version : {v}                    {Fore.red}#{Style.reset}
+                 {Fore.red}#{Style.reset}           Programmer    : EvilTwins                 {Fore.red}#{Style.reset}
+                 {Fore.red}#{Style.reset}           Facebook      : @evi1.twins               {Fore.red}#{Style.reset}
+                 {Fore.red}#{Style.reset}           Github        : @Evil-Twins-X             {Fore.red}#{Style.reset}
+                 {Fore.red}#{Style.reset}{Fore.green}====================================================={Style.reset}{Fore.red}#{Style.reset}
     """]
     return listBrand[0]
     
